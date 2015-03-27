4ca13d99a8cb3290cbaf3c4a9203129d0f99974d
ruyar4 <ruyar410@gmail.com>
2015-03-27 16:05:37 -0300

    fix2

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -8,6 +8,11 @@ class BidsController < ApplicationController
 	    @product = Product.find(params[:product_id])
 	    @bid = Bid.last
 	    if @bid.nil?
+	    	@product.price = @product.price + 500
+			Bid.create(bid_price:500,product:@product, user:current_user)
+			@product.save
+			redirect_to products_path
+	    else
 		    if current_user.id == @bid.user_id && @bid.product_id == @product.id
 		    	respond_to do |format|
 			        format.html { redirect_to products_path, notice: 'No puedes hacer dos pujas seguidas' }
